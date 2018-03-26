64e25a96b20a6dae66e4b8109def930f85d381a6
xyecs <runedesired2@gmail.com>
2018-03-26 01:36:40 -0400

    update lightsource, still black img..

--- a3/raytracer/light_source.cpp
+++ a3/raytracer/light_source.cpp
@@ -9,6 +9,7 @@
 #include <cmath>
 #include "light_source.h"
 
+
 void PointLight::shade(Ray3D& ray) {
 	// TODO: implement this function to fill in values for ray.col 
 	// using phong shading.  Make sure your vectors are normalized, and
@@ -19,5 +20,37 @@ void PointLight::shade(Ray3D& ray) {
 	// before this function.  
 
 	Intersection intersect = ray.intersection;
+
+	Material* mat = intersect.mat;
+
+	Vector3D normal = intersect.normal;
+
+	Vector3D viewVec = ray.dir;
+	
+
+	Point3D light_pos = get_position();
+	Vector3D lightDir = ( light_pos - intersect.point );
+	lightDir.normalize();
+
+	// https://en.wikipedia.org/wiki/Phong_reflection_model
+	Vector3D reflection = 2.0 * (normal.dot(lightDir)) * normal - lightDir;
+	reflection.normalize();
+
+	Color col = ray.col; 
+	Color ambient = mat->ambient * col_ambient;
+	
+	Color diffuse = fmax(0, lightDir.dot(normal)) * (mat->diffuse * col_diffuse);
+
+/* float spec = max(0.0, dot(viewVec, reflection));
+  vec3 specular = pow(spec, shininessVal) * specularColor * Ks;
+*/
+	float spec = fmax (0, viewVec.dot(reflection));
+	Color specular = pow(spec, mat->specular_exp) * (mat->specular * col_specular);
+
+	col.clamp();
+	
+	ray.col = ambient + diffuse + specular;
+
+
 }
 
